3d57b365efb691372296dd2eff328c9649976731
Gauravkumawat09 <gauravkumawatgk.09@gmail.com>
2024-11-27 12:49:12 +0530

    First commit

---
+++ chapter-1/first.py
@@ -0,0 +1,27 @@
+# print("hello world")
+# a = 1
+# b = 2
+# print(a+b)
+# t = type(a)
+# print(t)
+
+# c = input("Enter a Number :") 
+
+# print("First Number is:", c)
+# a = 11
+# for i in range(1,a):
+#     print(i)
+
+a = 10
+b = 5
+a = a + b
+b = a - b
+a = a- b
+print(a)
+print(b)
+
+print("hi", end="/")
+print("hello")
+print("pyhton")
+
+print("hi","rohit","sagar",sep="#")
